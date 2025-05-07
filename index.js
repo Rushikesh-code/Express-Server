e29de5f7d182fbf1256fb0231094c54783e83764
TheSBVaidya <sanjaybabanvaidya@gmail.com>
2025-05-07 16:05:54 +0530

    Favourite View All

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const userRouter = require("./routes/users");
 const quoteRouter = require("./routes/quotes");
+const favouriteRouter = require("./routes/favourites");
 
 // middleware to enable CORS -- add following resp headers
 const cors = require("cors");
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/users", userRouter);
 app.use("/quotes", quoteRouter);
+app.use("/favourites", favouriteRouter);
 
 const port = 3017;
 app.listen(port, "0.0.0.0", () => {
--- routes/favourites.js
+++ routes/favourites.js
@@ -4,6 +4,16 @@ const express = require("express");
 const router = express.Router();
 
 //viewAllFavorite
-router.get("/:userId", (req, resp) => {
-  db.query("SELECT  ");
+router.get("/viewAll", (req, resp) => {
+  db.query(
+    "SELECT f.id, q.id, q.author, q.contents, q.userId, q.createdTime, u.id, u.firstName, u.lastName, u.email, u.phoneno, u.address FROM favourite f INNER JOIN user u ON f.userId = u.id INNER JOIN quote q ON f.quoteId = q.id",
+    (err, result) => {
+      if (err) return resp.send(apiError(err));
+
+      // success
+      resp.send(apiSuccess(result));
+    }
+  );
 });
+
+module.exports = router;
