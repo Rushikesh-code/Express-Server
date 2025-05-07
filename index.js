b226fc85156d1012df3bf83c361b656f2e9ee136
TheSBVaidya <sanjaybabanvaidya@gmail.com>
2025-05-07 12:15:00 +0530

    Suceessfully created working quotes api

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const userRouter = require("./routes/users");
 const quoteRouter = require("./routes/quotes");
 
 // middleware to enable CORS -- add following resp headers
-// const cors = require("cors");
-// app.use(cors());
+const cors = require("cors");
+app.use(cors());
 
 app.use(express.json());
 app.use("/users", userRouter);
 app.use("/quotes", quoteRouter);
 
-const port = 3000;
+const port = 3017;
 app.listen(port, "0.0.0.0", () => {
   console.log("server ready at port", port);
 });
--- routes/quotes.js
+++ routes/quotes.js
@@ -14,6 +14,44 @@ router.get("/getAll", (req, resp) => {
 });
 
 // Edit Quote
+router.patch("/editQuote", (req, resp) => {
+  const { id, contents } = req.body;
+  db.query(
+    "UPDATE quote SET contents = ? WHERE id = ?",
+    [contents, id],
+    (err, result) => {
+      if (err) return resp.send(apiError(err));
+
+      if (result.affectedRows !== 1)
+        return resp.send(apiError("Content Not Updated"));
+      return resp.send(apiSuccess("Content is Updated"));
+    }
+  );
+});
+
+// Add Quote
+router.post("/addQuote", (req, resp) => {
+  const { author, contents, userId } = req.body;
+  db.query(
+    "INSERT INTO quote(author, contents, userId, createdTime) VALUES(?, ?, ?, NOW())",
+    [author, contents, userId],
+    (err, result) => {
+      if (err) return resp.send(apiError(err));
+
+      // if Success
+      if (result.affectedRows === 1) {
+        db.query(
+          "SELECT * FROM user WHERE id = ?",
+          [result.insertId],
+          (err, results) => {
+            if (err) return resp.send(apiError(err));
+            return resp.send(apiSuccess(results[0]));
+          }
+        );
+      }
+    }
+  );
+});
 
 // Delete Quote
 router.delete("/:id", (res, resp) => {
